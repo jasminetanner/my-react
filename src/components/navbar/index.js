f18b9449580422e1e782cbed36918df74ccfeebb
Jasmine <jasmine.tanner97@gmail.com>
2020-10-16 16:18:07 +0200

    fix(navbar): updated navbar

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Film } from 'react-bootstrap-icons';
 import {
   Col,
   Row,
@@ -13,20 +14,21 @@ const NavBar = () => (
   <Row>
     <Col>
       <Navbar bg="dark" variant="dark">
-        <svg width="1em" height="1em" viewBox="0 0 16 16" fill="white" xmlns="http://www.w3.org/2000/svg">
-          <path d="M0 1a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v14a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1V1zm4 0h8v6H4V1zm8 8H4v6h8V9zM1 1h2v2H1V1zm2 3H1v2h2V4zM1 7h2v2H1V7zm2 3H1v2h2v-2zm-2 3h2v2H1v-2zM15 1h-2v2h2V1zm-2 3h2v2h-2V4zm2 3h-2v2h2V7zm-2 3h2v2h-2v-2zm2 3h-2v2h2v-2z" />
-        </svg>
-        <Navbar.Brand href="#home"> MyMovies</Navbar.Brand>
+        <Navbar.Brand href="#home">
+          <Film />
+          <span className="ml-2">My Movies</span>
+        </Navbar.Brand>
         <Nav className="mr-auto">
-          <Nav.Link href="#action">Action</Nav.Link>
-          <Nav.Link href="#aventure">Aventure</Nav.Link>
-          <Nav.Link href="#horreur">Horreur</Nav.Link>
-          <Nav.Link href="#comedie">Comédie</Nav.Link>
-          <Nav.Link href="#sign-in">Sign-in</Nav.Link>
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#features">Action</Nav.Link>
+          <Nav.Link href="#pricing">Aventure</Nav.Link>
+          <Nav.Link href="#pricing">Horreur</Nav.Link>
+          <Nav.Link href="#pricing">Comédie</Nav.Link>
+          <Nav.Link href="#pricing">Sign-in</Nav.Link>
         </Nav>
         <Form inline>
           <FormControl type="text" placeholder="Search Movies" className="mr-sm-2" />
-          <Button variant="outline-info">Search</Button>
+          <Button variant="primary">Search</Button>
         </Form>
       </Navbar>
     </Col>
